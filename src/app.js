2803025df02a4306cd5ac3f75f56cf7d2f7853dc
ThiagoCCR <thiago.ccr96@gmail.com>
2022-09-15 13:54:21 -0300

    feat GETproducts, routers, controller && fix db.js

--- src/app.js
+++ src/app.js
@@ -3,9 +3,13 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 dotenv.config();
 import { PORT } from './configs/constants.js';
+import productsRouters from "./routers/productRouter.js"
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.listen(PORT, () => console.log(`Listen on ${PORT}`));
+app.use(productsRouters);
+
+
+app.listen(PORT, () => console.log(`Listening on ${PORT}`));
