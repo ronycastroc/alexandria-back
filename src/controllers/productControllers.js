599b070b386a55fe7915f2047c1e30f3171b80f8
ThiagoCCR <thiago.ccr96@gmail.com>
2022-09-15 14:43:34 -0300

    script

--- src/controllers/productControllers.js
+++ src/controllers/productControllers.js
@@ -12,4 +12,58 @@ const GetProducts = async (req, res) => {
   }
 };
 
-export { GetProducts };
+const FeedDB = async (req, res) => {
+  const booksDB = [
+    {
+      author: "J.R.R. Tolkien",
+      title: "O Senhor dos Aneis",
+      cover: "https://images-na.ssl-images-amazon.com/images/I/81SWBRKfExL.jpg",
+      category: "Fantasia",
+      price: "R$40,00",
+    },
+    {
+      author: "J.R.R. Tolkien",
+      title: "O Hobbit",
+      cover:
+        "https://images-na.ssl-images-amazon.com/images/I/511+-lOOtsL._SY344_BO1,204,203,200_.jpg",
+      category: "Fantasia",
+      price: "R$30,00",
+    },
+    {
+      author: "George R.R. Martin",
+      title: "A Guerra dos Tronos",
+      cover:
+        "https://images-na.ssl-images-amazon.com/images/I/41UKpOWrZVL._SY344_BO1,204,203,200_QL70_ML2_.jpg",
+      category: "Fantasia",
+      price: "R$35,00",
+    },
+    {
+      author: "Frank Herbert",
+      title: "Duna",
+      cover:
+        "https://images-na.ssl-images-amazon.com/images/I/41MRn6hy8-L._SY344_BO1,204,203,200_QL70_ML2_.jpg",
+      category: "Fantasia",
+      price: "R$45,00",
+    },
+    {
+      author: "Miguel Cervantes",
+      title: "Dom Quixote",
+      cover:
+        "https://images-na.ssl-images-amazon.com/images/I/51itDflUblL._SY344_BO1,204,203,200_QL70_ML2_.jpg",
+      category: "Fantasia",
+      price: "R$25,00",
+    },
+  ];
+  try {
+    // for (let i = 0; i < booksDB.length; i++) {
+    //   await db.collection("products").insertOne(booksDB[i]);
+    // }
+    booksDB.forEach(async(book)=>await db.collection("products").insertOne(book))
+    res.sendStatus(201);
+  } catch (error) {
+    console.error(error.message);
+    res.sendStatus(500);
+  }
+};
+
+export { GetProducts, FeedDB };
--- src/routers/productRouter.js
+++ src/routers/productRouter.js
@@ -1,9 +1,11 @@
-import { GetProducts } from "../controllers/productControllers.js";
+import { GetProducts, FeedDB } from "../controllers/productControllers.js";
 import express from "express";
 
 const router = express.Router();
 
 router.get("/products", GetProducts);
+
+router.post("/feedb", FeedDB)
 
 export default router;
 
