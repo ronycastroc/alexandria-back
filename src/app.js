a2f5befee17d52ff8eaa95b7cb1eea57499b38cd
ronycastroc <castroroony@gmail.com>
2022-09-18 05:42:59 -0300

    postcheckout done

--- src/app.js
+++ src/app.js
@@ -1,19 +1,18 @@
-import express, { application } from "express";
+import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 dotenv.config();
-import { PORT } from './configs/constants.js';
-import authRouter from './routers/authRoutes.js';
+import authRouters from "./routers/authRoutes.js";
 import productsRouters from "./routers/productRouter.js";
+import checkOutRouters from "./routers/checkoutRoute.js";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use(authRouter)
+app.use(authRouters);
 app.use(productsRouters);
-
-//app.listen(PORT, () => console.log(`Listening on ${PORT}`));
+app.use(checkOutRouters);
 
 app.listen(process.env.PORT, () => {
     console.log("Server running on port " + process.env.PORT);
